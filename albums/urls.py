02286b794b62e61e8c8b08c266687fe57f08061b
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2026-05-22 13:41:10 +0800

    feat: add clean minimalist user registration interface

--- albums/urls.py
+++ albums/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AlbumListView, AlbumCreateView, AlbumUpdateView, AlbumDeleteView
+from .views import AlbumListView, AlbumCreateView, AlbumUpdateView, AlbumDeleteView, SignUpView
 
 urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('', AlbumListView.as_view(), name='album-list'),
     path('new/', AlbumCreateView.as_view(), name='album-create'),
     path('<int:pk>/edit/', AlbumUpdateView.as_view(), name='album-edit'),
--- albums/views.py
+++ albums/views.py
@@ -1,7 +1,13 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import ListView, ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Album
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # Redirects to login page after successful registration
+    template_name = 'registration/signup.html' 
 
 # READ: List all albums
 class AlbumListView(ListView):
